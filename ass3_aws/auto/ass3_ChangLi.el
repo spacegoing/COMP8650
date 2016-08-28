13e7dffefe90e291401c404c567befa8074dfe0a
spacegoing <spacegoing@gmail.com>
2016-08-28 18:02:12 +1000

    Finished 4.8

---
+++ ass3_aws/auto/ass3_ChangLi.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "ass3_ChangLi"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "10pt" "a4paper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "amssymb"
+    "amsmath")
+   (LaTeX-add-bibliographies))
+ :latex)
+
