8b6a498426c520b2a35871ccbced02d9ce1648ae
spacegoing <spacegoing@gmail.com>
2016-10-02 20:00:23 +1100

    Plots Finished

--- ass5_aws/auto/ass5_ChangLi.el
+++ ass5_aws/auto/ass5_ChangLi.el
@@ -3,13 +3,21 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "10pt" "a4paper")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("mcode" "framed" "numbered" "autolinebreaks" "useliterate") ("graphicx" "pdftex")))
    (TeX-run-style-hooks
     "latex2e"
     "article"
     "art10"
     "amssymb"
     "amsmath"
-    "cite")
+    "cite"
+    "mcode"
+    "graphicx")
+   (LaTeX-add-labels
+    "fig:q2vs"
+    "fig:q3tradeoff"
+    "fig:q3recovered")
    (LaTeX-add-bibliographies))
  :latex)
 
